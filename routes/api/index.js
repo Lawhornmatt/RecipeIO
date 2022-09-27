62c8e2e8f67c0bb4b37d7283af00da71f056c00a
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-09-26 21:06:26 -0500

    Removed Routes

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const blahRoutes = require('./blah-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
-
-router.use('/blah', blahRoutes);
-
-module.exports = router;
