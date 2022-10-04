3dd7931eaf01ba013d73aa0d5359e83b962b1d81
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-10-04 06:01:27 -0500

    Create Recipe Page
    Is stylized as best I can for now

--- controllers/mattRoutes.js
+++ controllers/mattRoutes.js
@@ -38,19 +38,47 @@ router.post('/newbook',withAuth, async (req, res) =>{
 
 // ===  NEW RECIPE ROUTES ===
 
-router.get('/newrecipe',withAuth, (req, res) => {
+router.get('/newrecipe',withAuth, async (req, res) => {
 
     try {
-        res.render('newRecipe', {
-          logged_in: req.session.logged_in,
-        });
+      const userBooks = await Book.findAll( {
+        where: {
+          user_id: req.session.user_id
+        }
+      });
+
+      const preBookData = userBooks.map((book) => book.get({plain: true}));
+
+      res.render('newRecipe', {
+        preBookData,
+        logged_in: req.session.logged_in,
+      });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
 router.post('/newrecipe',withAuth, async (req, res) =>{
-    // console.log('POST /register | req.body: ' + req.body.firstName + ' ' + req.body.lastName + ' ' + req.body.username + ' ' + req.body.email + ' ' + req.body.password)
+    try {
+        const recipeData = await Recipe.create({
+          name: req.body.name,
+          ingredients: req.body.ingredients,
+          directions: req.body.directions,
+        });
+  
+        // console.log('POST /register | bookData' + bookData);
+  
+        req.session.save(() => {
+          req.session.logged_in = true;
+  
+          res.json({status: 'ok', message:`${bookData.name} is created!`})
+        });
+    }catch(err){
+        res.status(400).json(err);
+    }
+});
+
+router.post('/editbook',withAuth, async (req, res) =>{
     try {
         const bookData = await Book.create({
           name: req.body.name,
--- public/libs/logout.js
+++ public/libs/logout.js
@@ -9,7 +9,34 @@ const logout = async () => {
     } else {
       alert('you may not log out.');
     }
-  };
+};
   
-  document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector('#logout').addEventListener('click', logout);
+
+const genRecipe = async (event) => {
+    event.preventDefault();
   
+    const name = document.querySelector('#recipe-name').value.trim();
+    const ingredients = document.querySelector('#recipe-ingredients').value.trim();
+    const directions = document.querySelector('#recipe-directions').value.trim();
+  
+    if (name && ingredients && directions) {
+      const response = await fetch('/newrecipe', {
+        method: 'POST',
+        body: JSON.stringify({ name, ingredients, directions }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      // console.log(JSON.stringify({ name, ingredients, directions }))
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('you may not sign up.');
+      }
+    }
+};
+  
+document
+    .querySelector('#makeRecipe')
+    .addEventListener('click', genRecipe);
