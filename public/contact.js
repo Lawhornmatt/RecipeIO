adbfbdeaec1c96220f8172ded241f44fddaa7235
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-10-05 19:53:28 -0500

    delete contact handlebar and link contact.js to abount us

--- public/contact.js
+++ public/contact.js
@@ -1,19 +1,15 @@
-const firstName = document.querySelector('#first-name').value;
-const lastName = document.querySelector('#last-name').value;
-const subject = document.querySelector('#subject-line').value;
-const email = document.querySelector('#user-email').value;
-const message = document.querySelector('#user-message').value;
+const Name = document.querySelector('#customerName').value;
+const email = document.querySelector('#customerEmail').value;
+const message = document.querySelector('#customerMssg').value;
 const submit = document.querySelector('#submit');
 
 submit.addEventListener("click", function(event){
     event.preventDefault();
-    if (firstName && message) {
-        const recipe = {
-            firstName: firstName,
-            lastName: lastName,
-            subject: subject,
-            email: email,
-            message: message
+    if (Name && email && message) {
+        const userContact = {
+            Name,
+            email,
+            message
         };
     } else {
         alert("need first name and message to contact us")
